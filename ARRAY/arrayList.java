0a78bea282ad14dfe46328fbe0e0b17ff591fb7f
rituraj <riturajroj1234@gmail.com>
2022-06-27 23:15:48 +0530

    arraylist

--- ARRAY/arrayList.java
+++ ARRAY/arrayList.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
 public class arrayList {
     public static void main(String[] args){
-        System.out.println("hey");
+        //syntax for arrayList
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(69);
+        list.add(69);
+        list.add(69);
+        list.add(69);
+        list.add(69);
+        list.add(69);
+        list.add(69);
+        System.out.println(list);
+        System.out.println(list.contains(69));
     }
 }
 
