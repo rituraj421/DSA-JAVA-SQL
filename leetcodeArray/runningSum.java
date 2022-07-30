13d341ad0ce116cd6067886725380a23dbf87c6a
rituraj <riturajroj1234@gmail.com>
2022-07-30 22:16:52 +0530

    runningSum

--- leetcodeArray/runningSum.java
+++ leetcodeArray/runningSum.java
@@ -1,4 +1,15 @@
+package leetcodeArray;
 
-
+// import java.util.Scanner;
+public class runningSum {
+    public static int[] main(String[] args, int[] nums){
+        int[] ans = new int[nums.length];
+    ans[0] = nums[0];
+    for (int i = 1; i < nums.length; i++)
+        ans[i] = ans[i-1] + nums[i];
+    return ans;
+        // System.out.println("hey");
+    }
+}
 
 
