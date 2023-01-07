92009b833f90f5a4d2632cf3f54cdd4cec88fb32
rituraj roj <riturajroj1234@gmail.com>
2023-01-07 23:36:17 +0530

    leetcode

---
+++ leetcode/ARRAY/Easy/ListNode.java
@@ -0,0 +1,11 @@
+package leetcode.ARRAY.Easy;
+
+public class ListNode {
+
+    public ListNode(int i) {
+    }
+
+    public ListNode next;
+    public int val;
+
+}
---
+++ leetcode/ARRAY/Easy/removeLinkedListElements_203.java
@@ -0,0 +1,21 @@
+package leetcode.ARRAY.Easy;
+
+public class removeLinkedListElements_203 {
+    class Solution {
+        public ListNode removeElements(ListNode head, int val) {
+            ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = head;
+        ListNode prev = dummy;
+        while (current != null) {
+            if (current.val == val) {
+                prev.next = current.next;
+            } else {
+                prev = current;
+            }
+            current = current.next;
+        }
+        return dummy.next;
+    }
+    }    
+}
