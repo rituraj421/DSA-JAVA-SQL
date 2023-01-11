0ab12d5f00b952d220e436978431bc217bc4eb95
rituraj roj <riturajroj1234@gmail.com>
2023-01-11 23:13:39 +0530

    leetcode medium

--- CRASH/Lecture12string.java
+++ CRASH/Lecture12string.java
@@ -1,19 +1,23 @@
+package CRASH;
+
 import java.util.Scanner;
+
 public class Lecture12string {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println("Your name is : "+ name);
-        //concatenation
+        try (Scanner sc = new Scanner(System.in)) {
+            String name = sc.nextLine();
+            System.out.println("Your name is : " + name);
+        }
+        // concatenation
         String firstName = "ritu ";
         String lastName = " Choudhary";
-        String fullName = firstName + "@" +lastName;
+        String fullName = firstName + "@" + lastName;
         System.out.println(fullName.length());
-        //charAt
-        for(int i = 0; i<fullName.length(); i++){
+        // charAt
+        for (int i = 0; i < fullName.length(); i++) {
             System.out.println(fullName.charAt(i));
         }
-        //System.out.println();
+        // System.out.println();
         String sentence = "rituraj";
         String chomu = sentence.substring(3);
         System.out.println(chomu);
