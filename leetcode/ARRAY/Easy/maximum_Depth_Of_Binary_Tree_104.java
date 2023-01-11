1f531b09c275588e01db19882cf671da6cd32915
rituraj roj <riturajroj1234@gmail.com>
2023-01-11 23:13:26 +0530

    leetcode easy

---
+++ leetcode/ARRAY/Easy/maximum_Depth_Of_Binary_Tree_104.java
@@ -0,0 +1,25 @@
+package leetcode.ARRAY.Easy;
+
+public class maximum_Depth_Of_Binary_Tree_104 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int leftDepth = maxDepth(root.left);
+            int rightDepth = maxDepth(root.right);
+            return Math.max(leftDepth, rightDepth) + 1;
+        }
+    }
+
+}
