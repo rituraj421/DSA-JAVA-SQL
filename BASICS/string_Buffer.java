51bcf1774645bd1ff7e658f647b242aade885a36
rituraj roj <riturajroj1234@gmail.com>
2023-01-26 23:50:17 +0530

    java Basics

--- BASICS/string_Buffer.java
+++ BASICS/string_Buffer.java
@@ -31,5 +31,43 @@ public class string_Buffer {
         System.out.println(sb1 == sb2); // returns false
         System.out.println(sb1.equals(sb2)); // returns false
 
+        String sa = new String("rituraj");
+        sa.concat(" roj"); // garbage collection 
+        sa = sa.concat(" choudhary");
+        System.out.println(sa);
+
+        String r = new String("spring");
+        r.concat("Fall");
+        String r1 = r.concat(" winter");
+        r1.concat("Summer");
+
+        System.out.println(r);
+        System.out.println(r1);
+
+        System.out.println("Strings c series");
+
+        String c1 = new String("You cannot change me");
+        String c2 = new String("You cannot change me");
+
+        System.out.println(c1 == c2);
+
+        String c3 = "You cannot change me";
+        System.out.println(c1 == c3);
+
+        String c4 = "You cannot change me";
+        System.out.println(c3 == c4);
+
+        String c5 = "You cannot" + " change me"; // both constant true
+        System.out.println(c4 == c5);
+
+        String c6 = "You cannot";
+        String c7 = c6 + " change me"; // both are not constant , false 
+        System.out.println(c4 == c7);
+
+        final String c8 = "You cannot"; // final means constant, i.e both are constant so will return true
+        String c9 = c8 + " change me";
+        System.out.println(c4 == c9); // returns true
+
+
     }
 }
