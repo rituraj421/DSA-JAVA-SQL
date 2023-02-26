40fda18adfc873c181d0902a20d9b2a628a5ffda
rituraj roj <riturajroj1234@gmail.com>
2023-02-26 23:08:58 +0530

    fib

---
+++ BASICS/fib.java
@@ -0,0 +1,15 @@
+package BASICS;
+
+public class fib {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        int c;
+        for (int i = 0; i < 10; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.print(c);
+        }
+    }
+}
