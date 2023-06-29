6b52bb8410b748f78854eeeddb0158e914fee989
rituraj roj <riturajroj1234@gmail.com>
2023-06-29 22:37:28 +0530

    queue

---
+++ QUEUE/dequeue1.java
@@ -0,0 +1,16 @@
+package QUEUE;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class dequeue1 {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        deque.add(44);
+        deque.addFirst(45);
+        deque.removeFirst();
+
+        System.out.println(deque);
+    }
+}
---
+++ QUEUE/queue1.java
@@ -0,0 +1,18 @@
+package QUEUE;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class queue1 {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(3);
+        queue.add(5);
+        queue.add(6);
+        queue.add(15);
+        queue.add(19);
+
+        System.out.println(queue.peek());
+    }
+
+}
