b8cda871d5e0be638f9e2d6ad5fb2bedd5be28e8
rituraj roj <riturajroj1234@gmail.com>
2023-07-06 20:41:23 +0530

    ooops

--- SQL/Leetcode/Easy/CREATE DATABASE Leetcod_Easy;.sql
+++
@@ -1,9 +0,0 @@
--- CREATE DATABASE Leetcod_Easy;
-
--- USE Leetcod_Easy;
-
-CREATE TABLE q1 (
-    customer_id INT PRIMARY KEY,
-    name VARCHAR(50),
-    referee_id INT
-);
--- SQL/Leetcode/Easy/q2.sql
+++
@@ -1,7 +0,0 @@
--- USE leetcod_easy;
-
-CREATE TABLE q2 (
-    customer_id INT PRIMARY KEY,
-    name VARCHAR(50),
-    referee_id INT
-);
