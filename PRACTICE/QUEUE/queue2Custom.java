934dafc4cbbd032afdfc3cf07392daec082d409d
rituraj roj <riturajroj1234@gmail.com>
2023-07-31 23:27:06 +0530

    queue location change

---
+++ PRACTICE/QUEUE/queue2Custom.java
@@ -0,0 +1,78 @@
+package PRACTICE.QUEUE;
+
+public class queue2Custom {
+
+    private int[] data;
+    private static final int DEFAULT_SIZE = 10;
+
+    int end = 0;
+
+    public queue2Custom() {
+        this(DEFAULT_SIZE);
+    }
+
+    public queue2Custom(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean isFull() {
+        return end == data.length;
+    }
+
+    public boolean isEmpty() {
+        return end == 0;
+    }
+
+    public boolean insert(int item) {
+        if (isFull()) {
+            return false;
+        }
+        data[end++] = item;
+        return true;
+    }
+
+    public int remove() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        }
+
+        int removed = data[0];
+
+        // shift the elementa to left
+        for (int i = 1; i < end; i++) {
+            data[i - 1] = data[i];
+        }
+        end--;
+        return removed;
+    }
+
+    public int front() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        }
+        return data[0];
+    }
+
+    public void display() {
+        for (int i = 0; i < end; i++) {
+            System.out.print(data[i] + " <- ");
+        }
+        System.out.println("END");
+    }
+
+    public static void main(String[] args) throws Exception {
+        queue2Custom queue = new queue2Custom(5);
+
+        queue.insert(3);
+        queue.insert(6);
+        queue.insert(5);
+        queue.insert(19);
+        queue.insert(1);
+
+        queue.display();
+
+        System.out.println(queue.remove());
+
+        queue.display();
+    }
+}
