3a061dd6626b1d851c283c1491c96c7ff3dc43ad
rituraj roj <riturajroj1234@gmail.com>
2023-12-22 20:00:24 +0530

    fork_java_Program_1

---
+++ FORK_JAVA/f_1_hello.java
@@ -0,0 +1,11 @@
+package FORK_JAVA;
+
+public class f_1_hello {
+    public static void main(String[] args) {
+        // creating a simple java program which says "hello world"
+        System.out.println("Hello world");
+
+        // first compile the program using "javac f_1_hello.java" in terminal
+        // second run the program using "java f_1_hello"
+    }
+}
